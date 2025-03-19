24ae3352a57821cb32d9858ed5e67160f958454d
rubenleisfp <rubenleisfp@gmail.com>
2025-03-19 09:27:59 +0100

    Preparamos proyecto para subir

--- src/main/java/com/fp/cloudinary/CloudinaryApplication.java
+++ src/main/java/com/fp/cloudinary/CloudinaryApplication.java
@@ -1,10 +1,14 @@
 package com.fp.cloudinary;
 
 import com.fp.cloudinary.service.CloudinaryService;
+import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
 
 @SpringBootApplication
 public class CloudinaryApplication implements CommandLineRunner {
@@ -18,10 +22,10 @@ public class CloudinaryApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		Resource resource = new ClassPathResource("images/monkey.jpg");
-//
-//		// Convertir el recurso a un archivo
-//		File imageFile = resource.getFile();
-//		cloudinaryService.upload(imageFile,"samples/trash");
+		Resource resource = new ClassPathResource("images/monkey.jpg");
+
+		// Convertir el recurso a un archivo
+		File imageFile = resource.getFile();
+		cloudinaryService.upload(imageFile,"samples/trash");
 	}
 }
